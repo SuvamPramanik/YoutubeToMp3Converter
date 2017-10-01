5de5e036db593002218e30a08924cfbe3b2d3797
Himanshu Singh <himanshusingh2407@gmail.com>
2017-10-01 23:07:51 +0530

    Final API working

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var mp3 = require('./util/convertor.js');
 
 app.get('/', function(req, res){
   res.setHeader('Content-Type', 'application/json');
-  res.send(mp3.getMP3(req));
+  res.send(mp3.getMP3(req.query));
 });
 console.log('The app is running at PORT : ' + port);
 app.listen(port);
--- server/util/convertor.js
+++ server/util/convertor.js
@@ -2,34 +2,41 @@ var api = require('./api.js');
 
 function checkState(url) {
 
-  var response = api.callApi(url);
-  var output = response.split("|");
+  while (true) {
+    var response = api.callApi(url);
+    var output = [];
+    if (response) {
+      output = response.split("|");
+    }
+    console.log(response, url);
 
-  if (output[0] == "OK") {
-    return ("http://dl" + output[1] + ".downloader.space/dl.php?id=" + output[2]);
+    if (output[0] == "OK") {
+      return ("http://dl" + output[1] + ".downloader.space/dl.php?id=" + output[2]);
+    }
+
+    if (output[0] == "ERROR" && (output[1] == 1 || output[1] == 5)) {
+      return "ERROR";
+    }
+
+    // if (output[0] == "ERROR" && output[1] == "PENDING") {
+    //   return setTimeout(function(){ checkState(url) }, 5000);
+    // }
+    //
+    // if (output[0] == "DOWNLOAD") {
+    //   return setTimeout(function(){ checkState(url) }, 5000);
+    // }
   }
-
-  if (output[0] == "ERROR" && (output[1] == 1 || output[1] == 5)) {
-    return "ERROR";
-  }
-
-  if (output[0] == "DOWNLOAD") {
-    return output[2];
-  }
-
-  setTimeout(checkState(url), 5000);
 }
 
 module.exports = {
-  getMP3: function createResponse(req) {
-    var query = req.query;
-    // console.log(query);
-    let videoID = "uQ763VvqiEM";
+  getMP3: function createResponse(query) {
+    console.log('query::', query);
+    let videoID = query.videoID || "uQ763VvqiEM";
     let format = "mp3";
     let backendID = Math.floor(Math.random() * 3500000)
     let url = "check.php?v=" + videoID + "&f=" + format + "&h=" + backendID;
     // var response = api.callApi(url);
 
-    return checkState(url);
+    return {resultURL: checkState(url)};
   }
 };
