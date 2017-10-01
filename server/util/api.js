09c82e58189071b87324a6faf5aeee8e6f36b2df
Himanshu Singh <himanshusingh2407@gmail.com>
2017-10-01 23:29:54 +0530

    Add Heroku setup

--- server/util/api.js
+++ server/util/api.js
@@ -3,7 +3,7 @@ module.exports = {
   //Calls the api using GET method and returns the JSON response
   callApi: function(url) {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", process.env.api || "http://api.convert2mp3.cc/" + url, false);
+    xhttp.open("GET", process.env.api + url, false);
     xhttp.setRequestHeader("Content-type", "application/json");
     xhttp.send();
     return (xhttp.responseText);
--- server/util/convertor.js
+++ server/util/convertor.js
@@ -1,42 +1,31 @@
 var api = require('./api.js');
 
 function checkState(url) {
-
   while (true) {
     var response = api.callApi(url);
     var output = [];
     if (response) {
       output = response.split("|");
+      if (output[0] == "OK") {
+        return (process.env.apiProtocol + output[1] + process.env.apiMidUrl + output[2]);
+      }
+      if (output[0] == "ERROR" && (output[1] == 1 || output[1] == 5)) {
+        return "ERROR";
+      }
     }
-    console.log(response, url);
-
-    if (output[0] == "OK") {
-      return ("http://dl" + output[1] + ".downloader.space/dl.php?id=" + output[2]);
-    }
-
-    if (output[0] == "ERROR" && (output[1] == 1 || output[1] == 5)) {
-      return "ERROR";
-    }
-
-    // if (output[0] == "ERROR" && output[1] == "PENDING") {
-    //   return setTimeout(function(){ checkState(url) }, 5000);
-    // }
-    //
-    // if (output[0] == "DOWNLOAD") {
-    //   return setTimeout(function(){ checkState(url) }, 5000);
-    // }
   }
 }
 
 module.exports = {
   getMP3: function createResponse(query) {
     console.log('query::', query);
-    let videoID = query.videoID || "uQ763VvqiEM";
-    let format = "mp3";
-    let backendID = Math.floor(Math.random() * 3500000)
-    let url = "check.php?v=" + videoID + "&f=" + format + "&h=" + backendID;
-    // var response = api.callApi(url);
-
-    return {resultURL: checkState(url)};
+    let videoID = query.videoID;
+    if (videoID) {
+      let format = "mp3";
+      let backendID = Math.floor(Math.random() * 3500000)
+      let url = "check.php?v=" + videoID + "&f=" + format + "&h=" + backendID;
+      return {resultURL: checkState(url)};
+    } else
+      return {error: 'Empty or wrong query parameter.'};
   }
 };
